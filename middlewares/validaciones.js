224501123f28481339b8f988a98637fbebc2028c
Ricardo Cardoso <ricardo_71190@hotmail.com>
2023-02-23 07:58:57 -0600

    validador middleware

---
+++ middlewares/validaciones.js
@@ -0,0 +1,18 @@
+const {response} = require ('express'); //PARA IDENTIFICAR QUE ES UNA VARIABLE DE TIPO RESPONSE
+const {validationResult} = require ('express-validator');
+
+const validarCampos = (req, resp = response, next) => {
+
+    const errores = validationResult(req);
+
+    if(!errores.isEmpty()){
+        resp.status(400).json({
+            ok:false,
+            errors: errores.mapped()
+        })
+    }
+
+    next();
+}
+
+module.exports = {validarCampos}
