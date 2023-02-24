0be2866eba450959f7002cd5b64b2aab307ac302
Rkira7 <ricardo_71190@hotmail.com>
2023-02-23 22:17:00 -0600

    validar datos

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,11 +1,21 @@
 const {Router} = require('express');
 const {getUsuario, crearUsuario, actualizarUsuario, eliminarUsuario} = require('../controllers/usuarios');
+const {check} = require ('express-validator');
+const { validarCampos } = require('../middlewares/validaciones');
+
 
 const router = Router();
 
 router.get('/', getUsuario);
 
-router.post('/', crearUsuario);
+router.post('/', 
+    [
+        check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+        check('email', 'El correo es obligatorio').not().isEmpty().isEmail(),
+        check('password', 'El contraseña es obligatorio').not().isEmpty(),
+        validarCampos
+        
+    ], crearUsuario);
 
 router.put('/', actualizarUsuario)
 
